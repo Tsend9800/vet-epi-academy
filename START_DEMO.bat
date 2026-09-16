ac654bb16c922d26345ac1cd060010c6cf257bb9
Tsend9800 <epi.surveillance2024@gmail.com>
2026-09-16 15:38:14 +0800

    Add files via upload

---
+++ START_DEMO.bat
@@ -0,0 +1,8 @@
+﻿@echo off
+chcp 65001 >nul
+title VET EPI ACADEMY - Demo
+echo.
+echo  VET EPI ACADEMY - Мал эмнэлгийн тархвар зүйн сургалт
+echo  Демо хувилбарыг нээж байна...
+echo.
+start "" "%~dp0index.html"
